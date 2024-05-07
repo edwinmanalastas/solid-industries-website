abded55c3cb02e595af9f3ca361c2f819c4c1c16
edwinmanalastas <115379763+edwinmanalastas@users.noreply.github.com>
2024-05-06 19:04:55 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-/* mobile navigation bar */
-document.querySelector('.hamburger').addEventListener('click', function() {
-    document.querySelector('.nav-center').classList.toggle('active');
-    document.querySelector('.nav-right').classList.toggle('active');
-});
-
-
+/* navigation bar */
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
 
 const observer = new IntersectionObserver((entries)=> {
     entries.forEach((entry)=> {
@@ -20,3 +21,22 @@ const observer = new IntersectionObserver((entries)=> {
 const hiddenElements = document.querySelectorAll('.text-content, .about-header, .list-container, .services-header, .services .service, .choose-header, .choose-col, .reviews .container, .contact');
 hiddenElements.forEach((el)=> observer.observe(el));
 
+/* swipers */
+const swiper = new Swiper('.js-reviews-slider', {
+    grabCursor: true,
+    spaceBetween: 30,
+    pagination: {
+        el: '.js-reviews-pagination',
+        clickable: true
+    },
+    autoplay: {
+    delay: 10000, // Delay between transitions in milliseconds. Adjust as needed.
+    disableOnInteraction: false, // Continue autoplay after user interactions
+    },
+    speed: 1000,
+    breakpoints: {
+        767: {
+            slidesPerView: 2
+        }
+    }
+});
